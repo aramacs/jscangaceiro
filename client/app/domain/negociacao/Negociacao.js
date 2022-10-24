938a9404af6c978aa2b592b8be87dd0361b2e350
carlosonrails <sdluizcarlos.psn@gmail.com>
2022-10-24 14:23:53 -0300

    Estudando Imutabilidade

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,11 @@
 class Negociacao {
 
   constructor(data, quantidade, valor) {
-    this._data = data;
+    //previne a possibildade de se passar uma variavel  que instacie um new Date () para o construtor. 
+    //Sendo assim, não é possível
+    // a referência da variável para alterar a data da Negociacao
+    this._data = new Date(data.getTime());
+    
     this._quantidade = quantidade;
     this._valor = valor;
     Object.freeze(this);
@@ -10,9 +14,10 @@ class Negociacao {
   get volume() {
     return this._quantidade * this._valor;
   }
-
+  //Retorna um novo objeto Date toda vez que o getter date é acessado criando uma cópia do original 
+  //impedindo a mudança. Conceito de programação defensiva.
   get data() {
-    return this._data;
+    return new Date(this._data.getTime())
   }
 
   get quantidade() {
