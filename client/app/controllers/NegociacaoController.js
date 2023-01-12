57b0432932d8514974239575dfee4b3e26f74876
carlosonrails <sdluizcarlos.psn@gmail.com>
2023-01-12 12:51:32 -0300

    cap. 6 - finalizado lógica que de criaçao de negociacoes

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,25 +1,32 @@
 class NegociacaoController {    
   constructor() {
 
-    let $ = document.querySelector.bind(document);
-
+     let $ = document.querySelector.bind(document);
      this._inputData = $('#data');
      this._inputQuantidade = $('#quantidade');
      this._inputValor = $('#valor');
+     this._negociacoes = new Negociacoes();
   }
 
   adiciona(event) {
 
     event.preventDefault();
+    this._negociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario()
+  }
 
-// CHAMANDO MÉTODO ESTÁTICO
-    let negociacao = new Negociacao(DateConverter.paraData(this._inputData.value),
-    parseInt(this._inputQuantidade.value),
-    parseFloat(this._inputValor));
+  _limpaFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
+  }
 
-    console.log(negociacao);
-
-    let diaMesAno = DateConverter.paraTexto(negociacao.data);
-    console.log(diaMesAno)
+  _criaNegociacao() {
+    return new Negociacao(
+      DateConverter.paraData(this._inputData.value),
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
   }
 }
