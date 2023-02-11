07ded85b68185cfab1239fe51064e1a4d1767111
carlosonrails <sdluizcarlos.psn@gmail.com>
2023-02-11 20:53:05 -0300

    cap 10.1 aplicacao de Factory

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,26 +1,46 @@
-class NegociacaoController {    
+class NegociacaoController {
   constructor() {
 
-     const $ = document.querySelector.bind(document);
-     this._inputData = $('#data');
-     this._inputQuantidade = $('#quantidade');
-     this._inputValor = $('#valor');
-     // this._negociacoes = new Negociacoes(model => {
-     // this._negociacoesView.update(model);
-       // })
-     this._negociacoes = new Negociacoes();
-     this._negociacoesView = new NegociacoesView('#negociacoes');
-     this._negociacoesView.update(this._negociacoes);
-     this._mensagem = new Mensagem();
-     this._mensagemView = new MensagemView('#mensagemView')
-     this._mensagemView.update(this._mensagem);
+    const $ = document.querySelector.bind(document);
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+
+    const self = this;
+    this.negociacoes = new Proxy(new Negociacoes(), {
+
+
+      get(target, prop, receiver) {
+
+        if (typeof (target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
+          .includes(prop)) {
+
+          return function() {
+
+            console.log(`"${prop}" disparou a armadilha`);
+            target[prop].apply(target, arguments);
+            self._negociacoesView.update(target);
+          }
+
+        } else {
+          return target[prop];
+        }
+      }
+    });
+
+    this._negociacoesView = new NegociacoesView('#negociacoes');
+    this._negociacoesView.update(this._negociacoes);
+    
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView('#mensagemView')
+    this._mensagemView.update(this._mensagem);
   }
 
   adiciona(event) {
 
     event.preventDefault();
     this._negociacoes.adiciona(this._criaNegociacao());
-    this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+    this._mensagem.texto = 'Negociação adicionada com sucesso';
     this._mensagemView.update(this._mensagem)
     this._limpaFormulario();
   }
